76fc20c1d67eccbbc0944eb44471535b777f612b
hyungkyulee <hyungkyu@deepeyes.co.uk>
2024-03-16 14:33:17 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           height={37}
           priority
         />
-        <h2>Hello ...</h2>
+        <h2>My name is Riche...</h2>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
